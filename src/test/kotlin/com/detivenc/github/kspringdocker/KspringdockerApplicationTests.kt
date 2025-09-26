00361c53cfa02806b58134f820291ce0513916f5
detivenc <116792124+detivenc@users.noreply.github.com>
2025-09-26 23:54:36 +0200

    Delete src/test/kotlin/com/detivenc/github/kspringdocker/KspringdockerApplicationTests.kt

--- src/test/kotlin/com/detivenc/github/kspringdocker/KspringdockerApplicationTests.kt
+++
@@ -1,9 +0,0 @@
-package com.detivenc.github.kspringdocker
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class KspringdockerApplicationTests {
-
-}
